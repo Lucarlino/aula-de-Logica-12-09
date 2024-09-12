8508f4427ec997832fda026d4e5b5780279b19e7
juliaEkemilly <juulfreitas-kbeatrizcezar>
2024-09-12 15:45:15 -0300

    aula 12/09

--- nome.py
+++ nome.py
@@ -1,7 +1,8 @@
-nome = None 
-while True: 
-  nome = input("Digite seu nome ou pressione X para sair: ")
-  if nome == "x" or nome == "X":
-   print("Saindo do programa...")
-  elif nome == "Lucas":
-    print("Seja bem vindo Lucas!")
+while True:
+    nome = input("Digite seu nome ou aperte a tecla 'X' para sair ")
+    if nome == "x" or nome == "X":
+        break
+    else: 
+        print(f"Seja bem vindo, {nome}")
+
+print ('Até logo')
